71d7c7ef282a08dc6540eb918c1a9f6d934694a2
Pedro Magdaleno <jguillen.pedromagdaleno@gmail.com>
2019-10-20 23:34:12 +0200

    Add BillBusinessController #1

---
+++ src/main/java/es/upm/miw/apaw_gabriel_pedro/bill_resource/BillBusinessController.java
@@ -0,0 +1,33 @@
+package es.upm.miw.apaw_gabriel_pedro.bill_resource;
+
+import es.upm.miw.apaw_gabriel_pedro.exceptions.NotFoundException;
+import es.upm.miw.apaw_gabriel_pedro.product_data.Product;
+import es.upm.miw.apaw_gabriel_pedro.product_data.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BillBusinessController {
+
+    private BillDao billDao;
+    private ProductDao productDao;
+
+    @Autowired
+    public BillBusinessController(BillDao billDao, ProductDao productDao) {
+        this.billDao = billDao;
+        this.productDao = productDao;
+    }
+
+    public BillBasicDto create (BillCreationDto billCreationDto) {
+        Product myProduct;
+        for (int i = 0; i <= billCreationDto.getProducts().size() - 1; i++) {
+            myProduct = billCreationDto.getProducts().get(i);
+            Product finalMyProduct = myProduct;
+            Product product = this.productDao.findById(myProduct.getId())
+                    .orElseThrow(()-> new NotFoundException("Product id: "+ finalMyProduct.getId()));
+            System.out.println(billCreationDto.getProducts().get(i));
+        }
+        Bill bill = new Bill (billCreationDto.getTotal(), billCreationDto.getTotalIva(), billCreationDto.getProducts());
+        this.billDao.save(bill);
+        return new BillBasicDto(bill);
+    }
+
+}
