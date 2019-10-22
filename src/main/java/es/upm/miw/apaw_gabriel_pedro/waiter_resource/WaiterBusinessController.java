b1ab439d5c30d96a981c9a8ce94fb43a68fe1714
Pedro Magdaleno <jguillen.pedromagdaleno@gmail.com>
2019-10-22 20:58:51 +0200

    Add method delete waiter in controller #4

--- src/main/java/es/upm/miw/apaw_gabriel_pedro/waiter_resource/WaiterBusinessController.java
+++ src/main/java/es/upm/miw/apaw_gabriel_pedro/waiter_resource/WaiterBusinessController.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_gabriel_pedro.waiter_resource;
 
+import es.upm.miw.apaw_gabriel_pedro.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -15,5 +16,11 @@ public class WaiterBusinessController {
         Waiter waiter = new Waiter(waiterDto.getName(), waiterDto.getStarDate(), waiterDto.getBirthDate());
         this.waiterDao.save(waiter);
         return new WaiterDto(waiter);
+    }
+
+    public void delete(String id){
+        Waiter waiter = this.waiterDao.findById(id)
+                .orElseThrow(()-> new NotFoundException("Waiter id: "+ id));
+        this.waiterDao.delete(waiter);
     }
 }
