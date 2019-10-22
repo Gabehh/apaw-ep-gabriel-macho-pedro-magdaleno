0180ff82698dca16521f02b66503060c127cd3cf
Pedro Magdaleno <jguillen.pedromagdaleno@gmail.com>
2019-10-22 03:22:29 +0200

    Add method in BillBusinessController #3

--- src/main/java/es/upm/miw/apaw_gabriel_pedro/bill_resource/BillBusinessController.java
+++ src/main/java/es/upm/miw/apaw_gabriel_pedro/bill_resource/BillBusinessController.java
@@ -8,6 +8,9 @@ import es.upm.miw.apaw_gabriel_pedro.product_data.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class BillBusinessController {
 
@@ -31,6 +34,13 @@ public class BillBusinessController {
         Bill bill = new Bill (billCreationDto.getTotal(), billCreationDto.getTotalIva(), billCreationDto.getProducts());
         this.billDao.save(bill);
         return new BillBasicDto(bill);
+    }
+
+
+    public List<BillGetDto> findBills(BillGetDto billGetDto){
+        return this.billDao.findAll().stream()
+                .map(BillGetDto::new)
+                .collect(Collectors.toList());
     }
 
 }
