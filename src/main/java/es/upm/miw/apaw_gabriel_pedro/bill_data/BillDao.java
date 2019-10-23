1b18469b6ae6c26b86bbc8fe6d51d203ee7c2735
Gabriel Enrique Macho <gabe3195@gmail.com>
2019-10-23 22:58:09 +0200

    Clear Code Smell #17
    Signed-off-by: Gabriel Enrique Macho <gabe3195@gmail.com>

--- src/main/java/es/upm/miw/apaw_gabriel_pedro/bill_data/BillDao.java
+++ src/main/java/es/upm/miw/apaw_gabriel_pedro/bill_data/BillDao.java
@@ -1,6 +1,5 @@
 package es.upm.miw.apaw_gabriel_pedro.bill_data;
 
-import es.upm.miw.apaw_gabriel_pedro.bill_data.Bill;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BillDao extends MongoRepository<Bill, String> {
